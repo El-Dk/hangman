2c1c8e8917b695aa8c4e45746fb7c1f875bf68d1
Derek <briannish@gmail.com>
2021-09-02 22:24:23 -0400

    Add letter input validation

--- lib/main.rb
+++ lib/main.rb
@@ -11,17 +11,30 @@ def choose_word
   word.upcase
 end
 
+def clean_input(hangman)
+  letter = ''
+  loop do
+    letter = gets.chomp.upcase
+    break if letter.length == 1 && letter.match?(/[A-Z]/)
+    puts 'Invalid input!'
+    print 'Enter a new letter: '
+  end
+  loop do
+    break unless hangman.already_picked?(letter)
+    puts "ERROR! You've already picked that letter."
+    print 'Enter a new letter: '
+    letter = clean_input(hangman)
+  end
+  letter
+end
+
 a = Hangman.new(choose_word)
 puts a.show_guessed
 p a.word
 puts a.word.include?('a')
 a.show_hangman
 loop do
-  b = gets.chomp.upcase
-  loop do
-    break unless a.already_picked?(b)
-    b = gets.chomp.upcase
-  end
+  b = clean_input(a)
   a.take_a_guess(b)
   a.show_hangman
   a.show_guessed
